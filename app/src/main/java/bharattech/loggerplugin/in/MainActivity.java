61287366f4cc86319fe65fb11246d2413b7cae40
PREM SHANKAR UPADHYAY <50406436+premanish9@users.noreply.github.com>
2024-01-03 17:26:23 +0530

    fixes

---
+++ app/src/main/java/bharattech/loggerplugin/in/MainActivity.java
@@ -0,0 +1,18 @@
+package bharattech.loggerplugin.in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import bharattech.loggerplugin.loggerlibrary.LoggerMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      //  LoggerMessage.debugMessage("MainActivity","Hello");
+    }
+}
